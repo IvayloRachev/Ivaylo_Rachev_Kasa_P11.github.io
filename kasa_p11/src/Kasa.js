e2b3c87825c584f0514c28f09d5e646e186e27f0
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-03-06 10:20:56 +0100

    Update Kasa.js create Home page
    Création da la page Home

--- kasa_p11/src/Kasa.js
+++ kasa_p11/src/Kasa.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './css/styles.css';
+import './css/normalize.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
 
 function Kasa() {
   return (
     <div className="body-container">
-      <h1>Bonjour</h1>
+      <Router>
+        <main>
+          <Routes>
+            <Route index element={<Home/>}/>
+            <Route path="home" element={<Home/>}/>
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
