3b3ad0b066b82f9e707c1536f30e8c1b8f1e8f55
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-03-20 09:52:12 +0100

    Create About page
    Création de la page à propos

---
+++ kasa_p11/src/pages/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import HeroImage from '../assets/hero-home.jpg';
+import Dropdown from '../components/Dropdown';
+
+function About() {
+    return (
+        <div className='about'>
+            <section className='about-hero'>
+                <img src={HeroImage} alt="Hero" className='about-hero-image' />
+                <h1 className='about-hero-text'> </h1>
+            </section>
+            <section className='about-content'>
+                <Dropdown title="Fiabilité"
+                        content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+                        classArticle="about-content-dropdown"
+                        classTitle="about-content-dropdown-title"
+                        classContent="about-content-dropdown-content" />
+                <Dropdown title="Respect"
+                        content="La bienveillanvce fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+                        classArticle="about-content-dropdown"
+                        classTitle="about-content-dropdown-title"
+                        classContent="about-content-dropdown-content" />
+                <Dropdown title="Service" 
+                        content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+                        classArticle="about-content-dropdown"
+                        classTitle="about-content-dropdown-title"
+                        classContent="about-content-dropdown-content" />
+                <Dropdown title="Sécurité" 
+                        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité ébablis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standarts sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                        classArticle="about-content-dropdown"
+                        classTitle="about-content-dropdown-title"
+                        classContent="about-content-dropdown-content" />
+            </section>
+        </div>
+    )
+}
+
+export default About;
