2926c3b2fb1a7dd089ef654074fab49aef698c04
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-03-22 12:11:13 +0100

    Beginning of the Lodgement page
    Début de la page Lodgement

--- kasa_p11/src/pages/Lodgement.js
+++ kasa_p11/src/pages/Lodgement.js
@@ -1,3 +1,22 @@
 import React from 'react';
 import {useParams, Navigate} from 'react-router-dom';
-import lodgement from '../data/data';
+import lodgements from '../data/data';
+
+function Lodgement() {
+    let {lodgementId} = useParams();
+    let lodgement = lodgements.find((lodgement) => lodgement.id === lodgementId);
+
+    // Verification if the ID is exist
+    if(lodgement === undefined) {
+        return <Navigate to="/error"/>
+    }
+
+    let {pictures, title, tags, location, rating, host, equipments, description} = lodgement;
+    return (
+        <div className='lodgement'>
+            
+        </div>
+    )
+}
+
+export default Lodgement;
