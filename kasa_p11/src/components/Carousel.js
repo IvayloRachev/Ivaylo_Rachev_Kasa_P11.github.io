d123a477540e21eb6e0bbda9c4db188a6181c914
IvayloRachev <rachev.ivaylo@yahoo.fr>
2023-03-22 18:25:24 +0100

    Beginning carousel function
    Début du composant Carousel

--- kasa_p11/src/components/Carousel.js
+++ kasa_p11/src/components/Carousel.js
@@ -1,1 +1,22 @@
 import React from 'react';
+import {useState} from "react";
+import * as PropTypes from 'prop-types';
+import CarouselArrowLeft from '../assets/carousel-arrow-left.svg';
+import CarouselArrowRight from '../assets/carousel-arrow-right.svg';
+
+function Carousel({images, title}) {
+    let [currentImage, setCurrentImage] = useState(0);
+    let numberOfImages = images.legth;
+
+    return (
+        <div>
+            {images.map((image, index) => {
+                return (
+                    <div key={index} className='lodgement-carousel-image-wrapper'>
+                        {}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
